b7fd5452065b17603dcc650b92869841a45f6c6d
ofelipegoulart <ofelipegoulart@gmail.com>
2022-09-13 18:07:42 -0300

    2nd Screen Feature

--- src/App.js
+++ src/App.js
@@ -4,31 +4,54 @@ import API from './API.js'
 import { render } from '@testing-library/react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showComponent: false,
+      characters: [],
+    };
+    this._onButtonClick = this._onButtonClick.bind(this);
+    
+  }
 
-  state = {
-    characters: [],
+  _onButtonClick() {
+    this.setState({
+      showComponent: true,
+    });
   }
 
   async componentDidMount(){
     const response = await API.get('');
-    this.setState({ characters: response.data.results});
-  }
+    this.setState({ 
+      characters: response.data.results
+    });
+    }
 
+    
   render() {
     const { characters } = this.state;
+    const { showComponent } = this.state; 
     return (
       <div>
           <h1 className='title'>Rick and Morty's Characters</h1>
-          <div class='wrapper'>
+          <div style={{ display: (showComponent ? 'block' : 'none') }}>
+          <button className='btn-character' onClick={() => this.setState({showComponent: !showComponent})}>Go back</button>
+
+          </div>
+          {/* Pensar na tela secundária */}
+          <div class='wrapper' style={{display: (showComponent ? 'none' : 'grid')}}>
+            
             {characters.map(character => (
               <div className='card'>
                 <img className='image' src={character.image}></img>
                 <div className='information-card'>
                   <h3 className ='name-character' key={character.id}>{character.name}</h3>
-                  <p className='status-character'>Status: {character.status}</p>
+                  <p className='status-character'>Status: <span>{character.status}</span></p>
+                  <button className='btn-character' onClick={() => this.setState({ showComponent: !showComponent })}>See more</button>
                 </div>
               </div>
             ))}
+            
           </div>
           
       </div>
