f233e0a39bceedb2040fa22c501f512c853b3342
ofelipegoulart <ofelipegoulart@gmail.com>
2022-09-14 16:02:10 -0300

    2nd screen ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
 import React, { Component } from 'react';
 import API from './API.js'
+import Dialog from './components/Dialog';
+import CharacterItem from './components/Dialog';
 import { render } from '@testing-library/react';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showComponent: false,
       characters: [],
+      showComponent: false,
     };
-    this._onButtonClick = this._onButtonClick.bind(this);
-    
-  }
-
-  _onButtonClick() {
-    this.setState({
-      showComponent: true,
-    });
   }
 
   async componentDidMount(){
@@ -27,36 +21,26 @@ class App extends Component {
     });
     }
 
-    
   render() {
-    const { characters } = this.state;
-    const { showComponent } = this.state; 
+    const { characters, showComponent } = this.state;
     return (
       <div>
-          <h1 className='title'>Rick and Morty's Characters</h1>
-          <div style={{ display: (showComponent ? 'block' : 'none') }}>
-          <button className='btn-character' onClick={() => this.setState({showComponent: !showComponent})}>Go back</button>
-
-          </div>
-          {/* Pensar na tela secundária */}
-          <div class='wrapper' style={{display: (showComponent ? 'none' : 'grid')}}>
-            
-            {characters.map(character => (
+        <h1 className='title'>Rick and Morty's Characters</h1>
+        { showComponent && <Dialog button={() => this.setState({ showComponent: !showComponent })}/> }
+        <div class='wrapper'>
+        { !showComponent && characters.map(character => (
               <div className='card'>
                 <img className='image' src={character.image}></img>
                 <div className='information-card'>
                   <h3 className ='name-character' key={character.id}>{character.name}</h3>
-                  <p className='status-character'>Status: <span>{character.status}</span></p>
+                  <p className='status-character'>Status: {character.status}</p>
                   <button className='btn-character' onClick={() => this.setState({ showComponent: !showComponent })}>See more</button>
                 </div>
               </div>
             ))}
-            
-          </div>
-          
+        </div>
       </div>
-    )
-  }
+ )}
 };
 
 export default App;
---
+++ src/components/Dialog.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './../App.css';
+
+function Dialog({ name, tool, button}) {
+      return (
+        <div>
+            <div className='main-card'>
+                <div className='main-information-card'>
+                <img className='main-image'></img>
+                    <div className='main-data'>
+                        <h3 className ='main-name-character'>{name}</h3>
+                        <p className='main-status-character'>Episodes: {tool}</p> 
+                    </div>
+                </div>
+            <button className='back-btn' onClick={button}>Go back</button>
+            </div>
+        </div>
+      );
+  }
+  
+Dialog.defaultProps = {
+    name: "Designer",
+    tool: "Adobe XD"
+}
+
+export default Dialog;
